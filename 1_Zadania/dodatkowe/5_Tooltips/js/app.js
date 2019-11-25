dc112abe6f691148517a1f12b0cba9d236b6b7b7
adrian_kurek <adrian.kurek93@gmail.com>
2019-11-25 19:49:43 +0100

    Complete task 5_Tooltips

--- 1_Zadania/dodatkowe/5_Tooltips/js/app.js
+++ 1_Zadania/dodatkowe/5_Tooltips/js/app.js
@@ -1,3 +1,21 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+document.addEventListener("DOMContentLoaded", (event) => {
+  var tooltips = document.getElementsByClassName("tooltip");
+
+  for (var i = 0; i < tooltips.length; i++) {
+    tooltips[i].addEventListener("mouseover", function(event) {
+      var tooltipSpan = document.createElement("span");
+
+      tooltipSpan.classList.add("tooltipText");
+      tooltipSpan.innerHTML = this.dataset.text;
+
+      this.appendChild(tooltipSpan);
+    });
+
+    tooltips[i].addEventListener("mouseout", function(event) {
+      this.removeChild(this.firstElementChild);
+    });
+  }
+});
