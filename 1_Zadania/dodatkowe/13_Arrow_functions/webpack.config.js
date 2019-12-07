fa813900c201105892537af3d8c3e12ce2341cc0
adrian_kurek <adrian.kurek93@gmail.com>
2019-12-07 22:23:27 +0100

    Fix for 13_Arrow_functions

--- 1_Zadania/dodatkowe/13_Arrow_functions/webpack.config.js
+++ 1_Zadania/dodatkowe/13_Arrow_functions/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: ['./js/app.js', './js/zadanie00.js', './js/zadanie07.js', './js/zadanie08.js', './js/zadanie09.js'],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'js'),
     filename: 'out.js'
   }
 };
